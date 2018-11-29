88efb330d419ae206bcaefdb2ffd7618ae47d1fb
Matthew Drummond <mattatatter@gmail.com>
2018-11-29 10:33:56 -0600

    Update checking_account.hpp

--- checking_account.hpp
+++ checking_account.hpp
@@ -1,19 +1,17 @@
 /*Matthew Drummond
- *B333M439
- *Program #4
  * 
  * 		Pseudo Code
  * 	Class name: Checking_account
  * 		This class inherits data members and member functions from Account.
  * 		It represents a checking account that charges a fee per transaction.
-*	Data:
-* 		fee - the fee charged for each transaction
-* 	Mutator functions:
-* 		Constructor - Will create a new account with a given fee per transaction
-* 	Functions:
-* 		credit - after Account.credit is called the fee will be subtracted
-* 		debit - if Account.debit is successful the fee will be subtracted
-*/
+ *	Data:
+ * 		fee - the fee charged for each transaction
+ * 	Mutator functions:
+ * 		Constructor - Will create a new account with a given fee per transaction
+ * 	Functions:
+ *  		credit - after Account.credit is called the fee will be subtracted
+ * 		debit - if Account.debit is successful the fee will be subtracted
+ */
 
 #include "account.hpp"
 
